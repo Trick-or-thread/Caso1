08f8091209b45ebe35079d72a8f53822a3a1e5e4
unknown <s.carrero@uniandes.edu.co>
2020-02-22 21:02:23 -0500

    Añadida documentación

--- src/Caso/Cliente.java
+++ src/Caso/Cliente.java
@@ -4,10 +4,20 @@ import java.util.Random;
 
 public class Cliente extends Thread {
 	
+	/**
+	 * Mensaje que va a dejar el cliente
+	 */
 	Mensaje mensaje;
 	
+	/**
+	 * Buffer para enviar mensaje a los servidores
+	 */
 	Buffer buffer;
 	
+	/**
+	 * Metodo constructor del cliente	
+	 * @param pBuffer Buffer para enviar mensaje a los servidores
+	 */
 	public Cliente(Buffer pBuffer) {
 		
 		mensaje = new Mensaje(new Random().nextInt());
@@ -16,6 +26,9 @@ public class Cliente extends Thread {
 		
 	}
 	
+	/**
+	 * Metodo para enviar un mensaje al buffer, si no lo logra permanece en espera activa.
+	 */	
 	public void enviar() {
 		
 		while (!buffer.dejarMensaje(mensaje)) {}
