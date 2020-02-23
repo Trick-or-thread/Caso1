d346bc68a257ba1ec45cda34b6f6eb8073cce581
Juan Fernando <jf.castaneda@uniandes.edu.co>
2020-02-22 22:08:37 -0500

    ola

--- src/Caso/Servidor.java
+++ src/Caso/Servidor.java
@@ -10,22 +10,6 @@ public class Servidor extends Thread {
 	}
 	
 	public void leer() {
-<<<<<<< HEAD
-=======
-
-		Mensaje msj = null;
-		
-		while((msj = buffer.sacarMensaje()) == null) {
-			
-			yield();			
-		}
-		
-		msj.setMensaje(msj.getMensaje() + 1);
-		
-		msj.notify();
-		
-	}
->>>>>>> 10f790ad7f7d1ee6cbbb1e5c48210353724fa4dc
 
 		Mensaje msj = null;
 		
