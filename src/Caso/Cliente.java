25c91c29efc7e806233d87356d499994229cd513
unknown <s.carrero@uniandes.edu.co>
2020-02-22 21:43:11 -0500

    Holis

--- src/Caso/Cliente.java
+++ src/Caso/Cliente.java
@@ -4,10 +4,6 @@ import java.util.Random;
 
 public class Cliente extends Thread {
 	
-	/**
-	 * Mensaje que va a dejar el cliente
-	 */
-	Mensaje mensaje;
 	
 	/**
 	 * Buffer para enviar mensaje a los servidores
@@ -15,13 +11,16 @@ public class Cliente extends Thread {
 	Buffer buffer;
 	
 	/**
+	 * Cantidad de mensajes a ser enviados
+	 */
+	int cantidadMensajes;
+	
+	/**
 	 * Metodo constructor del cliente	
 	 * @param pBuffer Buffer para enviar mensaje a los servidores
 	 */
 	public Cliente(Buffer pBuffer) {
-		
-		mensaje = new Mensaje(new Random().nextInt());
-		
+				
 		buffer = pBuffer;
 		
 	}
@@ -29,7 +28,7 @@ public class Cliente extends Thread {
 	/**
 	 * Metodo para enviar un mensaje al buffer, si no lo logra permanece en espera activa.
 	 */	
-	public void enviar() {
+	public void enviar(Mensaje mensaje) {
 		
 		while (!buffer.dejarMensaje(mensaje)) {
 			
@@ -44,5 +43,20 @@ public class Cliente extends Thread {
 		} catch (InterruptedException e) {}
 	}
 	
+	/**
+	 * Metodo que ejecuta el thread al hacer start 
+	 */
+	@Override
+	public void run() {
+		
+		for(int i = 0; i < cantidadMensajes; i++) {
+			
+			Mensaje mensaje = new Mensaje(new Random().nextInt());
+			
+			enviar(mensaje);
+
+		}		
+
+	}
 		
 }
